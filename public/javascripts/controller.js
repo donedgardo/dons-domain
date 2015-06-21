a0fb4caf8b4eee24016da28db7e6b46f1fcfc13c
donedgardo <edgardo.g.carreras@gmail.com>
2015-06-21 03:54:37 -0400

    Added Chat Ctrl and some bootstrap layout for testing

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -2,4 +2,16 @@ angular.module("donDomain")
 .controller('MainCtrl', [ '$scope', function ($scope){
 	self = this;
 	$scope.test = "Nav bar";
+}])
+.controller('ChatCtrl', ['$scope', function($scope){
+	self = this;
+	self.chat= [];
+	
+	$scope.sendChatMsg = function(){
+		if($scope.input_message){
+			console.log("Pushing msg to chat list");
+			self.chat.push(this.input_message);
+			$scope.input_message = "";
+		}
+	};
 }]);
